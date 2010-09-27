6bee8aea4503416eecaf1563fbac16268da7df10
Andrea Salicetti <andrea.salicetti@gmail.com>
2010-09-27 12:21:30 +0200

    Fix su recupero privo di params[:user] su query versioni

--- app/helpers/versioni_helper.rb
+++ app/helpers/versioni_helper.rb
@@ -1,7 +1,7 @@
 module VersioniHelper
   def filtra_segnalazioni(versione)
     segnalazioni = versione.segnalazioni || []
-    segnalazioni = segnalazioni.select {|s| s.involved_as_resolver(current_user)} if params[:user]
+    segnalazioni = segnalazioni.select {|s| s.involved_as_resolver(current_user)} if (params[:user] || params[:user] == nil)
     segnalazioni.sort{|x, y| (x.cda_tipo_segna <=> y.cda_tipo_segna)*100000 + x.prg_segna <=> y.prg_segna}
   end
 end
